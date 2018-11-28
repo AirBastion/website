530c729946a5b82051b1ad07d7c562294e83b823
Sachin Nikam <sachin.nikam@neosofttech.com>
2018-11-28 14:53:46 +0530

    Added layout for slide 6, Fixed margin issue on slide 7

--- src/components/ICO/SixthSlide/index.js
+++ src/components/ICO/SixthSlide/index.js
@@ -1,26 +1,58 @@
-import React, { Component } from 'react';
-import './index.css';
-import 'bulma/css/bulma.css';
+import React, { Component } from "react";
+import "./index.css";
+import "bulma/css/bulma.css";
+import styled from "styled-components";
 
-class SixthSlide extends Component{
-  constructor(){
+const Titled = styled.h1`
+  font-size: 2.3em;
+  text-align: left;
+  color: palevioletred;
+  line-height: 1.4em;
+`;
+
+class SixthSlide extends Component {
+  constructor() {
     super();
-
   }
 
-  render(){
+  render() {
     return (
+      <React.Fragment>
         <section className="section">
-            <div className="columns is-multiline is-desktop is-centered is-vcentered has-text-centered">
-                <div className="column is-full has-text-centered">
-                    <h1 className="is-size-3 has-text-weight-bold has-text-white">
-                        List of References
-                    </h1>
-                </div>
+          <div className="columns is-variable is-centered">
+            <div className="column">
+              <Titled>List of references</Titled>
             </div>
+          </div>
         </section>
+
+        <div className="section">
+          <div className="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
+            <div className="column is-10">
+              <div className="columns">
+                <div className="column is-5 box">
+                  First Title <br />
+                  Second Title <br />
+                  Third Title <br />
+                  <div className="column is-4 box">
+                    First Title <br />
+                    Second Title <br />
+                    Third Title <br />
+                  </div>
+                </div>
+                <div className="column is-6">
+                  <div className="columns">
+                    <div className="column box">Date Input</div>
+                    <div className="column box">Date Picker</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
-};
+}
 
 export default SixthSlide;
