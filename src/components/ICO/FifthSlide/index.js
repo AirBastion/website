74f380a45816f9a796d89eda6b028f80be7bb10a
Sachin Nikam <sachin.nikam@neosofttech.com>
2018-12-22 16:01:15 +0530

    Modified the interpolation for the curve of the timeline slide.

--- src/components/ICO/FifthSlide/index.js
+++ src/components/ICO/FifthSlide/index.js
@@ -29,18 +29,27 @@ class FifthSlide extends Component{
   render(){
       const sampleData = [
         { x: 0, y: 1 },
-        { x: 3.5, y: 1 },
-        { x: 4.49, y: 1.30 },
-        { x: 5.55, y: 1.6 },
-        { x: 6.25, y: 2.1 },
-        { x: 6.8, y: 2.6 },
-        { x: 7.4, y: 3.2 },
-        { x: 8.2, y: 4.1 },
-        { x: 8.6, y: 4.6 },
-        { x: 8.9, y: 5 },
-        { x: 10.3, y: 5.7 },
-        { x: 13, y: 5.9 },
+        { x: 2.25, y: 1 },
+        { x: 3.55, y: 1.3 },
+        { x: 4.5, y: 1.7 },
+        { x: 5.3, y: 2.2 },
+        { x: 6, y: 3.15 },
+        { x: 6.65, y: 4.2 },
+        { x: 7.32, y: 5.32 },
+        { x: 8.14, y: 6.15 },
+        { x: 8.95, y: 6.68 },
+        { x: 10, y: 7.06 },
+        { x: 12, y: 7.1 }
       ]
+
+      const testData = [
+        sampleData[1],
+        sampleData[4],
+        sampleData[7],
+        sampleData[10],
+        
+      ]
+
     return (
         <section className="section">
             <div className="columns is-multiline is-desktop is-centered is-vcentered has-text-centered">
@@ -56,12 +65,17 @@ class FifthSlide extends Component{
                             //groupComponent={<VictoryClipContainer clipPadding={{ top: 5, right: 10 }} />}
                             style={{ data: { stroke: "#5f53be", strokeWidth: 1 } }}
                             data={sampleData}
-                            interpolation="natural"
+                            interpolation="cardinal"
+                        />
+                        <VictoryScatter
+                            style={{ data: { fill: "#5f53be" } }}
+                            size={2}
+                            data={sampleData}
                         />
                         <VictoryScatter
                             style={{ data: { fill: "#fcfcfc" } }}
-                            size={2}
-                            data={sampleData}
+                            size={3}
+                            data={testData}
                         />
                     </VictoryChart>
                 </div>
