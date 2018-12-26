b7990a3f972c15ea00922b375976cf77a2ade8f5
Sachin Nikam <sachin.nikam@neosofttech.com>
2018-12-26 15:26:41 +0530

    Implemented the designings for the navigation bar with the required animations.

--- src/components/ICO/Carousal/index.js
+++ src/components/ICO/Carousal/index.js
@@ -4,8 +4,6 @@ import './index.css'
 class Carousal extends Component {
   render() {
       let {slides, carousalClick, selectIndex} = this.props;
-      console.log("selectIndex----------from carousal component-----", this.props.selectIndex)
-      
     return (
         <div className='carousal'>
             <div id='project-list'>
@@ -15,20 +13,20 @@ class Carousal extends Component {
                     let selectedDiv = index === selectIndex ? "list-items-horizontal" :"list-items";
                     return(
                     <div 
-                        onClick={()=>carousalClick(index)} 
+                        onClick={ ()=> carousalClick(index)} 
                         key={"nav-"+index}
-                        className={selectedDiv}>|
-                     {/* <span key={"line"+index} id={"line"+index}>{"#"+(index+1)}</span> */}
-                            {/* <span 
-                            id={"bar"+index}
-                            key={"bar"+index}>{index+1}
-                            </span> */}
+                        className={selectedDiv}>
+                            <span 
+                                id={"bar"+index}
+                                key={"bar"+index}>
+                                {"#"+(index+1)}
+                            </span>
+                            |
                         </div>
                     )
                    }
                    )
                }
-               
             </div>
         </div>
     );
